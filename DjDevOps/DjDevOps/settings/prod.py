d99cb54266d5505b41bb7928611f42b76cb80595
itachi44 <diopbamba86@gmail.com>
2023-08-31 16:40:27 +0200

    adding tests

--- DjDevOps/DjDevOps/settings/prod.py
+++ DjDevOps/DjDevOps/settings/prod.py
@@ -8,13 +8,13 @@ DEBUG=False
 
 DATABASES = {
 	'default': {
-			'ENGINE':'django.db.backends.postgresql',
+			'ENGINE': 'django.db.backends.postgresql',
 			'NAME' : os.getenv('POSTGRES_DB'),
 			'USER': os.getenv('POSTGRES_USER'),
 			'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
 			'HOST': os.getenv('DATABASE_PROD_HOST'),
 			'PORT': '5432',
-            'ATOMIC_REQUESTS': True
+        	'ATOMIC_REQUESTS': True
 	}
 }
 
--- DjDevOps/users/views.py
+++ DjDevOps/users/views.py
@@ -3,7 +3,6 @@ from .models import *
 
 # Create your views here.
 
-
 def index(request):
     users = Account.objects.all()
     return render(request, 'users/index.html', {'users': users})
